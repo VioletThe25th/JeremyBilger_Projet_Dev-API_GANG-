86c924bfee6f17c6960fbc77c4352424c6bc07bb
Tristiti <tristan.castelain@gmail.com>
2022-10-11 17:05:21 +0200

    Pipi

---
+++ app/Http/Controllers/GangController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gang;
+use App\Http\Requests\StoreGangRequest;
+use App\Http\Requests\UpdateGangRequest;
+
+class GangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreGangRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gang  $gang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gang $gang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gang  $gang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gang $gang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateGangRequest  $request
+     * @param  \App\Models\Gang  $gang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGangRequest $request, Gang $gang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gang  $gang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gang $gang)
+    {
+        //
+    }
+}
