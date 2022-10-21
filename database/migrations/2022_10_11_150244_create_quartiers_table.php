6b76258014ec032a0219d30a05527b72b8c836f6
Tristan <tristan.castelain@gmail.com>
2022-10-21 22:46:52 +0200

    Update 2022_10_11_150244_create_quartiers_table.php

--- database/migrations/2022_10_11_150244_create_quartiers_table.php
+++ database/migrations/2022_10_11_150244_create_quartiers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('quartiers', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 55);
             $table->timestamps();
         });
     }
